7504c9a295b8247809a6d5a22638d26bdf03117b
James Macmillan <jamescodesthings@gmail.com>
2018-02-13 14:10:31 +0000

    indentation and clearing

--- www/recaptcha.js
+++ www/recaptcha.js
@@ -1,43 +1,46 @@
-// Old es5 method version of the plugin for plain cordova projects.
 var recaptcha = {
   verify: function(api_key, callback, fail) {
     if (!cordova || !cordova.exec) {
       fail('Cannot find Cordova');
       return;
     }
+
     cordova.exec(function(result) {
-        if (typeof callback === 'function')
-          callback(result);
-      }, function(error) {
-        if (typeof fail === 'function')
-          fail(error);
-      },
-      'Recaptcha', 'verify', [api_key]
+      if (typeof callback === 'function') {
+        callback(result);
+      }
+    }, function(error) {
+      if (typeof fail === 'function') {
+        fail(error);
+      }
+    },
+    'Recaptcha', 'verify', [api_key]
     );
-  }
+  },
 };
 
-// Similar method to ionic-native, add the service if ng1 is active here.
 if (window.angular) {
   window.angular
     .module('cordova.recaptcha', [])
-    .service('$cordovaRecaptcha', ['$q', function($q){
+    .service('$cordovaRecaptcha', ['$q', function($q) {
       return {
         verify: function(site_key) {
           var deferred = $q.defer();
 
-          recaptcha.verify(site_key,
+          recaptcha.verify(
+            site_key,
             function(response) {
               deferred.resolve(response);
             },
             function(error) {
               deferred.reject(error);
-            });
+            }
+          );
 
           return deferred.promise;
-        }
+        },
       };
-    }])
+    }]);
 }
 
 module.exports = recaptcha;
